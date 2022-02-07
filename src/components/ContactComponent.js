d7c9f349b10721d4eec931a0a4f94453ceec3363
Trần Trường Sơn <95740163+SonCrits@users.noreply.github.com>
2022-02-07 12:05:24 +0700

    uncontrolled form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -28,6 +28,7 @@ class Contact extends Component {
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleBlur = this.handleBlur.bind(this);
+        
     }
 
     handleInputChange(event) {
@@ -48,43 +49,51 @@ class Contact extends Component {
     handleBlur = (field) => (evt) => {
         this.setState({
             touched : {...this.state.touched,[field] : true}
-        })
+        });
     }
 
     validate(firstname,lastname,telnum,email) {
         const errors = {
-            firstname:'',
-            lastname:'',
-            telnum:'',
-            email:''
+            firstname : '',
+            lastname : '',
+            telnum : '',
+            email : ''
         }
 
-        if(this.state.touched.firstname && firstname.length < 3){
-            errors.firstname = "fist name should be >= 3 character";
+        if(this.state.touched.firstname && firstname.length < 3) {
+            errors.firstname = 'first name should be >= 3 character';
         } else if(this.state.touched.firstname && firstname.length > 10) {
-            errors.firstname = "fist name should be <= 10 character"
+            errors.firstname = 'first name should be <= 10 character';
         }
 
-        if(this.state.touched.lastname && lastname.length < 3){
-            errors.lastname = "last name should be >= 3 character";
+        if(this.state.touched.lastname && lastname.length < 3) {
+            errors.lastname = 'last name should be >= 3 character';
         } else if(this.state.touched.lastname && lastname.length > 10) {
-            errors.lastname = "last name should be <= 10 character"
+            errors.lastname = 'last name should be <= 10 character';
         }
 
         const reg = /^\d+$/;
         if(this.state.touched.telnum && !reg.test(telnum)){
-            errors.telnum = "tel num should contain only numbers";
+            errors.telnum = 'tel num should contain only numbers';
         }
 
-        if(this.state.touched.email && email.split('').filter(x => x=== '@').length !== 1){
-            errors.email = "email should contain a @";
+        if(this.state.touched.email && email.split('').filter(x => x === '@').length !== 1){
+            errors.email = 'email should contain a @';
         }
 
         return errors;
+
+
     }
+   
     render() {
 
-        const errors = this.validate(this.state.firstname, this.state.lastname, this.state.telnum, this.state.email)
+        const errors = this.validate(
+            this.state.firstname,
+            this.state.lastname,
+            this.state.telnum,
+            this.state.email
+        )
         return(
             <div className="container">
                 <div className='row'>
