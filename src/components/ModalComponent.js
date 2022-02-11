2e5b62a6bfdfab65beacc18ac3da5cc0563d0b70
Trần Trường Sơn <95740163+SonCrits@users.noreply.github.com>
2022-02-11 15:07:15 +0700

    Validate Form Ok

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -5,8 +5,14 @@ import { Button, Form, FormGroup, Input, Label, Modal, ModalBody, ModalHeader, C
 function ButtonModal (props) {
     const [openModal , setOpenModal] = useState(false);
     const [newStaff,setNewStaff] = useState({ });
-    const [nameError, setNameError] = useState('');
-    // const values = '';
+    const [isDisabled, setIsDisabled] = useState(true);
+
+    let [nameError, setNameError] = useState("Required");
+    let [doBError, setDoBError] = useState("Required");
+    let [startDateError, setStartDateError] = useState("Required");
+    let [salaryScaleError, setSalaryScaleError] = useState("");
+    let [annualLeaveError, setAnnualLeaveError] = useState("");
+    let [overTimeError, setOverTimeError] = useState("");
 
 
     const toggleModal = () => {
@@ -16,7 +22,7 @@ function ButtonModal (props) {
     const hanleInputChange = (event) => {
         let staffKey = event.target.name;
         let value = event.target.value; 
-        newStaff[staffKey] = value;       
+        newStaff[staffKey] = value;      
     }
 
     function handleSubmit(event) {      
@@ -32,13 +38,58 @@ function ButtonModal (props) {
     }
 
     function checkValid() {
-        const isInvalid = false;
-        if(!newStaff.name || newStaff.name.length < 4){
-            setNameError('Yêu cầu nhập nhiều hơn 3 ký tự');
-            isInvalid = true;
+        let isInvalid = false;
+
+        // Check name valid
+        if (!newStaff.name || newStaff.name.length < 4) {
+        setNameError("Yêu cầu nhập nhiều hơn 3 ký tự");
+        isInvalid = true;
         } else {
-            setNameError("");
+        setNameError("");
         }
+
+         // Check date of birth valid
+    if (!newStaff.doB) {
+        setDoBError("Yêu cầu bắt buộc");
+        isInvalid = true;
+      } else {
+        setDoBError("");
+      }
+  
+      // Check start date valid
+      if (!newStaff.startDate) {
+        setStartDateError("Yêu cầu bắt buộc");
+        isInvalid = true;
+      } else {
+        setStartDateError("");
+      }
+  
+      if (
+        Number(newStaff.salaryScale) &&
+        newStaff.salaryScale >= 1 &&
+        newStaff.salaryScale <= 3
+      ) {
+        setSalaryScaleError("");
+      } else {
+        setSalaryScaleError("Yêu cầu nhập số từ 1-3");
+        isInvalid = true;
+      }
+  
+      if (Number(newStaff.annualLeave) || newStaff.annualLeave === "0") {
+        setAnnualLeaveError("");
+      } else {
+        setAnnualLeaveError("Yêu cầu nhập số");
+        isInvalid = true;
+      }
+  
+      if (Number(newStaff.overTime) || newStaff.overTime === "0") {
+        setOverTimeError("");
+      } else {
+        setOverTimeError("Yêu cầu nhập số");
+        isInvalid = true;
+      }
+
+        setIsDisabled(isInvalid);
     }
 
     return(
@@ -54,9 +105,13 @@ function ButtonModal (props) {
                             <Label md={5}>Tên</Label>
                             <Col md={7}>
                                 <Input type="text" name="name"
-                                    onChange={hanleInputChange}
+                                    onChange={(e) => {
+                                        hanleInputChange(e);
+                                        checkValid();
+                                    }}
                                     valid={nameError === ''}
-                                    invalid={nameError !== ''}/>
+                                    invalid={nameError !== ''}
+                                />
                                 <FormFeedback>{nameError}</FormFeedback>
                             </Col>
                         </FormGroup>
@@ -64,21 +119,39 @@ function ButtonModal (props) {
                             <Label md={5}>Ngày sinh</Label>
                             <Col md={7}>
                                 <Input type="date" name="doB"
-                                    onChange={hanleInputChange}/>
+                                    onChange={(e) => {
+                                        hanleInputChange(e);
+                                        checkValid();
+                                    }}
+                                    valid={nameError === ''}
+                                    invalid={nameError !== ''}
+                                />
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label md={5}>Ngày vào công ty</Label>
                             <Col md={7}>
                                 <Input type="date" name="startDate"
-                                    onChange={hanleInputChange}/>
+                                   onChange={(e) => {
+                                    hanleInputChange(e);
+                                    checkValid();
+                                }}
+                                valid={nameError === ''}
+                                invalid={nameError !== ''}
+                            />
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label md={5}>Phòng ban</Label>
                             <Col md={7}>
                                 <Input type="select" name='department'
-                                    onChange={hanleInputChange}>
+                                    onChange={(e) => {
+                                        hanleInputChange(e);
+                                        checkValid();
+                                    }}
+                                    valid={nameError === ''}
+                                    invalid={nameError !== ''}
+                                >
                                     <option selected disabled value={''}>
                                         chọn phòng ban
                                     </option>
@@ -94,21 +167,39 @@ function ButtonModal (props) {
                             <Label md={5}>Hệ số lương</Label>
                             <Col md={7}>
                                 <Input type="number" name="salaryScale"
-                                    onChange={hanleInputChange}/>
+                                    onChange={(e) => {
+                                        hanleInputChange(e);
+                                        checkValid();
+                                    }}
+                                    valid={nameError === ''}
+                                    invalid={nameError !== ''}
+                                />
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label md={5}>Số ngày nghỉ còn lại</Label>
                             <Col md={7}>
                                 <Input type="number" name="annualLeave"
-                                    onChange={hanleInputChange}/>
+                                    onChange={(e) => {
+                                        hanleInputChange(e);
+                                        checkValid();
+                                    }}
+                                    valid={nameError === ''}
+                                    invalid={nameError !== ''}
+                                />
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label md={5}>Số giờ làm thêm</Label>
                             <Col md={7}>
                                 <Input type="number" name="overTime"
-                                    onChange={hanleInputChange}/>
+                                    onChange={(e) => {
+                                        hanleInputChange(e);
+                                        checkValid();
+                                    }}
+                                    valid={nameError === ''}
+                                    invalid={nameError !== ''}
+                                />
                             </Col>
                         </FormGroup>
                         <FormGroup row>
