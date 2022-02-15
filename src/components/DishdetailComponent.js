961988ae8fd4e0741bcb293a322b2ad2e2d9ca89
Trần Trường Sơn <95740163+SonCrits@users.noreply.github.com>
2022-02-15 16:40:18 +0700

    Lab09_2 Ok

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,CardTitle,
 } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import CommentForm from './CommentFormComponent';
+import { Loading } from './LoadingComponent';
 
 
 function RenderDish ({dish}) {
@@ -51,7 +52,25 @@ function RenderComment({comments, dishId, addComment}) {
 }
 
 const DishDetail = (props) => {
-  if(props.dish != null) {
+  if(props.isLoading) {
+    return(
+      <div className='container'>
+        <div className='row'>
+          <Loading />
+        </div>
+      </div>
+    )
+  }
+  else if(props.errMess) {
+    return(
+      <div className='container'>
+        <div className='row'>
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    )
+  }
+  else if(props.dish != null) {
     return(
       <div className='container'>
         <div className='row'>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import { Redirect, Route, Switch,withRouter } from 'react-router-dom';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import {connect} from 'react-redux';
-import { addComment } from '../redux/ActionCreator';
+import { addComment, fetchDishes } from '../redux/ActionCreator';
 
 
 // hàm map state từ redux sang react
@@ -24,12 +24,17 @@ const mapStateToProps = state => {
 // hàm map dispatch từ redux sang
 const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) =>
-    dispatch(addComment(dishId, rating, author, comment))
+    dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => {dispatch(fetchDishes())}
 })
 
 class Main extends Component{
   constructor(props){
     super(props);
+  }
+
+  componentDidMount() {
+    this.props.fetchDishes();
   }
 
   render() {
@@ -38,7 +43,9 @@ class Main extends Component{
     const HomePage = () => {
       return(
         <Home
-          dish = {this.props.dishes.filter(dish => dish.featured)[0]}
+          dish = {this.props.dishes.dishes.filter(dish => dish.featured)[0]}
+          dishesLoading = {this.props.dishes.isLoading}
+          dishesErrMess = {this.props.dishes.errMess}
           promotion = {this.props.promotions.filter(promotion => promotion.featured)[0]}
           leader = {this.props.leaders.filter(leader => leader.featured)[0]} />
       )
@@ -48,8 +55,10 @@ class Main extends Component{
     const DishWithId = ({match}) => {
       return(
         <DishDetail
-          dish = {this.props.dishes.filter(
+          dish = {this.props.dishes.dishes.filter(
             (dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+          dishesLoading = {this.props.dishes.isLoading}
+          dishesErrMess = {this.props.dishes.errMess}
           comments = {this.props.comments.filter(
             (comment) => comment.dishId  === parseInt(match.params.dishId,10))} 
           addComment = {this.props.addComment}
